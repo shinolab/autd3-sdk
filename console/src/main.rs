ab5f05fc782ae52157ac13465a204b10533698cc
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-06-28 11:40:23 +0900

    chore(console): add application icon

--- console/src/main.rs
+++ console/src/main.rs
@@ -8,11 +8,15 @@ mod process;
 use eframe::egui;
 
 fn main() -> eframe::Result {
+    let mut viewport = egui::ViewportBuilder::default()
+        .with_inner_size([760.0, 560.0])
+        .with_min_inner_size([560.0, 400.0])
+        .with_title("AUTD3 Console");
+    if let Ok(icon) = eframe::icon_data::from_png_bytes(include_bytes!("../assets/icon.png")) {
+        viewport = viewport.with_icon(icon);
+    }
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([760.0, 560.0])
-            .with_min_inner_size([560.0, 400.0])
-            .with_title("AUTD3 Console"),
+        viewport,
         ..Default::default()
     };
     eframe::run_native(
