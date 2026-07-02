8a035ed0b3112a6714545d735156f17d39dbcd2b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-07-02 14:09:55 +0900

    chore(emulator): sync with pattern buffer and commands API changes

--- emulator/examples/src/bin/sound_field.rs
+++ emulator/examples/src/bin/sound_field.rs
@@ -13,11 +13,10 @@ use std::time::Duration;
 use anyhow::Result;
 use polars::prelude::{CsvWriter, SerWriter};
 
+use autd3_rs::commands::{Pattern, SetSilencer};
 use autd3_rs::geometry::{Autd3, Geometry, offset};
-use autd3_rs::params::NUM_TRANSDUCERS;
 use autd3_rs::units::{m, mm, s};
 use autd3_rs::value::Emission;
-use autd3_rs::{Pattern, SetSilencer};
 
 use autd3_rs_emulator::{ClientApi, Emulator, InstantRecordOption, RangeXY};
 
@@ -29,7 +28,8 @@ fn main() -> Result<()> {
 
     let target = geometry.center() + offset(0.0 * mm, 0.0 * mm, 150.0 * mm);
     let wavelength = autd3_rs_pattern::wavelength(340.0 * m / s);
-    let mut patterns = vec![[Emission::default(); NUM_TRANSDUCERS]; geometry.len()];
+    let mut patterns =
+        vec![vec![Emission::default(); Autd3::NUM_TRANSDUCERS]; geometry.num_devices()];
     autd3_rs_pattern::focus(
         &geometry,
         target,
