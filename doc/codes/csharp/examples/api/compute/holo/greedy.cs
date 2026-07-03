414c3285520140afad219381bb5cfe58dfbb1786
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-07-03 14:10:08 +0900

    refactor(cs): nest C# holo ControlPoint under Holo and add NalgebraBackend

--- doc/codes/csharp/examples/api/compute/holo/greedy.cs
+++ doc/codes/csharp/examples/api/compute/holo/greedy.cs
@@ -14,8 +14,8 @@ internal static class Sample
         var center = geometry.Center + new Vector3(0.0f, 0.0f, 150.0f);
         var foci = new[]
         {
-            new HoloControlPoint(center + new Vector3(-30.0f, 0.0f, 0.0f), 2.5e3f * Pa),
-            new HoloControlPoint(center + new Vector3(30.0f, 0.0f, 0.0f), 2.5e3f * Pa),
+            new Holo.ControlPoint(center + new Vector3(-30.0f, 0.0f, 0.0f), 2.5e3f * Pa),
+            new Holo.ControlPoint(center + new Vector3(30.0f, 0.0f, 0.0f), 2.5e3f * Pa),
         };
 
         var wavelength = Pattern.Wavelength(340.0f * m / s);
