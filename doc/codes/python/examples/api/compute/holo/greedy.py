05de32bbb2cdb916e33827848337b6769b78b997
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-07-03 15:02:51 +0900

    docs: remove wavelength sample and fix python wavelength shadowing

--- doc/codes/python/examples/api/compute/holo/greedy.py
+++ doc/codes/python/examples/api/compute/holo/greedy.py
@@ -1,7 +1,7 @@
 import numpy as np
 from autd3.geometry import Autd3, Geometry
 from autd3.units import m, s
-from autd3_pattern import wavelength
+from autd3_pattern import wavelength as calc_wavelength
 from autd3_pattern_holo import (
     ControlPoint,
     Directivity,
@@ -26,7 +26,7 @@ foci = [
     ),
 ]
 
-wl = wavelength(340 * m / s)
+wavelength = calc_wavelength(340 * m / s)
 option = (
     # ANCHOR: option
     GreedyOption(
@@ -39,5 +39,5 @@ option = (
 )
 dst = geometry.pattern_buffer()
 # ANCHOR: api
-greedy(geometry, foci, wl, option, dst)
+greedy(geometry, foci, wavelength, option, dst)
 # ANCHOR_END: api
--- doc/codes/python/examples/api/compute/pattern/bessel.py
+++ doc/codes/python/examples/api/compute/pattern/bessel.py
@@ -4,14 +4,14 @@ import numpy as np
 from autd3.geometry import Autd3, Geometry
 from autd3.units import m, rad, s
 from autd3.value import Intensity, Phase
-from autd3_pattern import BesselOption, bessel, wavelength
+from autd3_pattern import BesselOption, bessel, wavelength as calc_wavelength
 
 geometry = Geometry([Autd3([0.0, 0.0, 0.0], [1.0, 0.0, 0.0, 0.0])])
 
 apex = geometry.center() + np.array([0.0, 0.0, 150.0])
 direction = np.array([0.0, 0.0, 1.0])
 theta = math.radians(18.0) * rad
-wl = wavelength(340 * m / s)
+wavelength = calc_wavelength(340 * m / s)
 option = (
     # ANCHOR: option
     BesselOption(
@@ -23,5 +23,5 @@ option = (
 dst = geometry.pattern_buffer()
 
 # ANCHOR: api
-bessel(geometry, apex, direction, theta, wl, option, dst)
+bessel(geometry, apex, direction, theta, wavelength, option, dst)
 # ANCHOR_END: api
