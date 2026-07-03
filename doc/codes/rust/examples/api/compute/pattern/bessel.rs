bab0feb8fe38c95763494fea64a1311ba25c0b95
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-07-03 14:31:08 +0900

    docs: use Vector3::z_axis() instead of manual unit vector construction

--- doc/codes/rust/examples/api/compute/pattern/bessel.rs
+++ doc/codes/rust/examples/api/compute/pattern/bessel.rs
@@ -1,4 +1,4 @@
-use autd3_rs::geometry::{Autd3, Geometry, UnitVector3, Vector3, offset};
+use autd3_rs::geometry::{Autd3, Geometry, Vector3, offset};
 use autd3_rs::units::{deg, m, mm, s};
 use autd3_rs::value::{Emission, Intensity, Phase};
 use autd3_rs_pattern::{BesselOption, bessel, wavelength};
@@ -7,7 +7,7 @@ fn main() {
     let geometry = Geometry::new(vec![Autd3::default()]);
 
     let apex = geometry.center() + offset(0.0 * mm, 0.0 * mm, 150.0 * mm);
-    let direction = UnitVector3::new_normalize(Vector3::new(0.0, 0.0, 1.0));
+    let direction = Vector3::z_axis();
     let theta = 18.0 * deg;
     let wavelength = wavelength(340.0 * m / s);
     let option =
