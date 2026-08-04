8bbf180ca89534c2f891f64220e774bf6e19b2ef
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-08-04 11:06:48 +0900

    test: exercise the resync paths against the vendored firmware via Audit fault injection

--- crates/autd3-rs-firmware-emulator/src/device.rs
+++ crates/autd3-rs-firmware-emulator/src/device.rs
@@ -3,6 +3,7 @@ use autd3_rs_core::protocol::{RX_FRAME_BYTES, RxFrame, TX_FRAME_BYTES};
 use crate::emu_fpga::FpgaEmulator;
 use crate::fw;
 use autd3_cpu_fw::Cpu;
+use autd3_cpu_fw::proto::Mode;
 
 const WIRE_GAP_START: usize = fw::WIRE_RX_GAP_START;
 const WIRE_RX_FRAME_BYTES: usize = fw::WIRE_RX_FRAME_BYTES;
@@ -21,16 +22,37 @@ impl Device {
         Self { cpu, fpga }
     }
 
-    pub fn send(&mut self, tx: &[u8; TX_FRAME_BYTES]) -> RxFrame {
+    pub fn recv(&mut self, tx: &[u8; TX_FRAME_BYTES]) {
         let wire = logical_to_wire(tx);
         self.cpu.recv_ethercat(&mut self.fpga, &wire);
-        self.cpu.process_pending(&mut self.fpga);
+    }
 
+    pub fn process_one(&mut self) -> bool {
+        self.cpu.process_one(&mut self.fpga)
+    }
+
+    pub fn process_pending(&mut self) {
+        self.cpu.process_pending(&mut self.fpga);
+    }
+
+    #[must_use]
+    pub fn rx(&self) -> RxFrame {
         let tx = self.cpu.tx();
         let mut rx = [0u8; RX_FRAME_BYTES];
         rx[0] = tx.ack;
         rx[1] = tx.data;
         RxFrame::parse(&rx)
+    }
+
+    pub fn send(&mut self, tx: &[u8; TX_FRAME_BYTES]) -> RxFrame {
+        self.recv(tx);
+        self.process_pending();
+        self.rx()
+    }
+
+    #[must_use]
+    pub fn mode(&self) -> Mode {
+        self.cpu.mode()
     }
 
     #[must_use]
