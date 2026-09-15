418f0bf1fe08ef5d9e2b73473fa88859c2a9d39d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-09-15 09:41:24 +0900

    fix(fpga): make the output mask independent of the bank

--- firmware/fpga/rtl/sources_1/new/memory/interfaces/output_mask_bus_if.sv
+++ firmware/fpga/rtl/sources_1/new/memory/interfaces/output_mask_bus_if.sv
@@ -2,12 +2,11 @@
 `default_nettype none
 interface output_mask_bus_if ();
 
-  logic BANK;
   logic [255:0] VALUE;
   logic RD_EN;
 
-  modport in_port(input BANK, output VALUE, input RD_EN);
-  modport out_port(output BANK, input VALUE, output RD_EN);
+  modport in_port(output VALUE, input RD_EN);
+  modport out_port(input VALUE, output RD_EN);
 
 endinterface
 `default_nettype wire
